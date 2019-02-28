1cc00fc908ffc68a15ff632c03ec6216295570be
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-03-01 01:27:55 +0300

    A lot of changes and it works GREAT!!! everything is awesome!
    Took 9 hours 39 minutes

--- ft_hook.c
+++ ft_hook.c
@@ -20,9 +20,11 @@ void put_info(void *param)
 	char 	t[5][50];
 	t_img	img;
 
+	printf("test\n");
 	mngr = ((t_manager**)param)[1];
 	img = mngr->imgs[mngr->cur_img];
 	mlx = *(((t_mlx**)param)[2]);
+	//TODO repalce with lib ft_printf
 	sprintf(t[0], "Number of iterations: %zu, %d", img.iter, img.iter_mod);
 	sprintf(t[1], "Base color: (%f, %f, %f)", img.col.x, img.col.y, img.col.z);
 	sprintf(t[2], "Scale: 1:%.0f", (float)(1 / img.start.z));
@@ -44,18 +46,30 @@ size_t ft_get_iters(t_double3 start, int iter_mod)
 	return (ret);
 }
 
-void ft_redraw(void *param)
+int get_win(int x, int y)
+{
+	if (x > 0 && x < RES && y > 0 && y < RES)
+		return (MAIN_I);
+	if (x > RES && x < RES + RES / FRACTAL_PRV && y > 0)
+		return ((int)(y / ((float)RES / FRACTAL_PRV) + 2));
+	return (-1);
+}
+
+void ft_redraw(void *param, int nimg)
 {
 	t_mlx mlx;
 	t_manager *mngr;
 	t_img	*img;
+	t_double2 *jc;
 
 	mlx = *(((t_mlx**)param)[2]);
 	mngr = ((t_manager**)param)[1];
-	img = &mngr->imgs[mngr->cur_img];
+	img = &mngr->imgs[nimg];
 	img->iter = ft_get_iters(img->start, img->iter_mod);
-	ft_ocl_make_img(img, ((t_ocl**)param)[0]);
-	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr[mlx.cw], img->img_ptr, 0, 0);
+	jc = &mngr->jc;
+	ft_ocl_make_img(img, ((t_ocl**)param)[0], jc);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr[mlx.cw], img->img_ptr,
+			img->pos.x, img->pos.y);
 	if (mngr->info)
 		put_info(param);
 }
@@ -85,7 +99,7 @@ int		hook_keydwn(int key, void *param)
 	printf("key mask = %d\n", mngr->key_mask);
 	printf("CNTRL? %d, SHIFT? %d\n", IS_CNTRL_DOWN, IS_SHIFT_DOWN);
 	if (key == 53)
-		exit(0);
+		frct_close(param);
 	if (ITERS_PLUS || ITERS_MINUS)
 	{
 		if (!IS_CNTRL_DOWN && !IS_SHIFT_DOWN)
@@ -115,7 +129,7 @@ int		hook_keydwn(int key, void *param)
 		mngr->imgs[mngr->cur_img].start = (t_double3){-2.15, 1.5, 3.0 / RES};
 		mngr->imgs[mngr->cur_img].col = (t_float3){0.0, 0.6, 1.0};
 	}
-	ft_redraw(param);
+	ft_redraw(param, mngr->cur_img);
 	return (0);
 }
 
@@ -123,33 +137,47 @@ int mouse_move_handle(int x, int y, void *param)
 {
 	t_manager *mngr;
 	t_double3	*v;
+	int			win;
+	t_img		*img;
 
 	mngr = ((t_manager**)param)[1];
+	img = &mngr->imgs[mngr->cur_img];
+	printf("x = %d, y = %d\n", x , y);
 	v = &mngr->imgs[mngr->cur_img].start;
-	if(mngr->mouse_mask & (1 << 0))
+	printf("win %d start x = %f, y = %f, z = %f\n",win, v->x , v->y, v->z);
+	if (mngr->mouse_mask & (1 << 2) &&
+	IN_RANGE_II(img->pos.x, x, img->pos.x + img->res.x) &&
+	IN_RANGE_II(img->pos.y, y, img->pos.y + img->res.y))
+	{
+		mngr->jc = img->kern >= JULIA ? (t_double2){mngr->jc.x + (x - mngr->mse_mv_crd[2].x) * img->start.z / 6,
+													mngr->jc.y + (y - mngr->mse_mv_crd[2].y) * img->start.z / 6} : mngr->jc;
+		mngr->mse_mv_crd[2] = (t_int2){x, y};
+	}
+	else if(mngr->mouse_mask & (1 << 0))
 	{
 		v->x += mngr->mse_mv_crd[0].x != x ?
 				(mngr->mse_mv_crd[0].x - x) * v->z : 0;
 		v->y -=	(mngr->mse_mv_crd[0].y != y) ?
 				   -(mngr->mse_mv_crd[0].y - y) * v->z : 0;
 		mngr->mse_mv_crd[0] = (t_int2){x, y};
-		ft_redraw(param);
 	}
-	if(mngr->mouse_mask & (1 << 1))
+	else if(mngr->mouse_mask & (1 << 1) && mngr->cur_img == MAIN_I)
 	{
 		if (IS_SHIFT_DOWN)
-			mngr->imgs[mngr->cur_img].col.z = (float)x / RES;
+			mngr->imgs[mngr->cur_img].col.z += mngr->mse_mv_crd[1].x != x ?
+											   (float)(mngr->mse_mv_crd[1].x - x) / RES : 0;
 		else
 		{
-			mngr->imgs[mngr->cur_img].col.x = (float) x / RES;
-			mngr->imgs[mngr->cur_img].col.y = (float) y / RES;
+			mngr->imgs[mngr->cur_img].col.x += mngr->mse_mv_crd[1].x != x ?
+											   (float)(mngr->mse_mv_crd[1].x - x) / RES : 0;
+			mngr->imgs[mngr->cur_img].col.y -= mngr->mse_mv_crd[0].y != y ?
+					(float)(mngr->mse_mv_crd[1].y - y) / RES : 0;
 		}
-		ft_redraw(param);
+		mngr->mse_mv_crd[1] = (t_int2){x, y};
 	}
-	if(mngr->mouse_mask & (1 << 1))
-		printf("m2 pressed move: x = %d, y = %d\n", x, y);
-	if(mngr->mouse_mask & (1 << 2))
-		printf("m3 pressed move: x = %d, y = %d\n", x, y);
+	else
+		return (0);
+	ft_redraw(param, mngr->cur_img);
 	return (0);
 }
 
@@ -163,35 +191,68 @@ int		mouse_release(int but, int x, int y, void *param)
 	return (0);
 }
 
+void swap_img(t_img *small, t_img *main)
+{
+	t_double3	start_tmp;
+	int 		tmp;
 
+	printf("smal n = %d, main n = %d\n", small->kern, main->kern);
+	small->col = (t_float3){0, 0, 0};
+	start_tmp = small->start;
+	small->start = main->start;
+	main->start = start_tmp;
+	small->start.z *= FRACTAL_PRV;
+	main->start.z /= FRACTAL_PRV;
+	tmp = small->kern;
+	small->kern = main->kern;
+	main->kern = tmp;
+	tmp = small->iter;
+	small->iter = main->iter;
+	main->iter = tmp;
+	tmp = small->iter_mod;
+	small->iter_mod = main->iter_mod;
+	main->iter_mod = tmp;
+	printf("smal n = %d, main n = %d\n", small->kern, main->kern);
+}
 
 int		mouse_hook(int but, int x, int y, void *param)
 {
 	t_double3 *v;
 	t_manager *mngr;
+	t_img		*img;
+	int tmp;
 
 	mngr = ((t_manager**)param)[1];
-	v = &mngr->imgs[mngr->cur_img].start;
+	tmp = get_win(x, y);
+	mngr->cur_img = tmp < 0 ? 0 : tmp;
+	img = &mngr->imgs[mngr->cur_img];
+	v = &img->start;
 	if (y > 0 && (but == 3 || but == 2 || but == 1))
 	{
 		mngr->mouse_mask |= 1 << (but - 1);
 		mngr->mse_mv_crd[but - 1] = (t_int2){x, y};
+		if (but == 2 && mngr->cur_img != MAIN_I)
+		{
+			swap_img(img, &mngr->imgs[MAIN_I]);
+			ft_redraw(param, MAIN_I);
+			ft_redraw(param, mngr->cur_img);
+		}
 	}
 	if (but == 4 || but == 5)
 	{
 		if (but == 4)
 		{
-			v->x = v->x + x * (v->z - v->z / MOUSE_SCROL_SCALE);
-			v->y =  v->y + y * (v->z - v->z / MOUSE_SCROL_SCALE);
+			v->x = v->x + (x - img->pos.x) * (v->z - v->z / MOUSE_SCROL_SCALE);
+			v->y = v->y + (y - img->pos.y) * (v->z - v->z / MOUSE_SCROL_SCALE);
 			v->z /= MOUSE_SCROL_SCALE;
 		}
 		else
 		{
-			v->x = v->x + x * (v->z - v->z * MOUSE_SCROL_SCALE);
-			v->y =  v->y + y * (v->z - v->z * MOUSE_SCROL_SCALE);
+			v->x = v->x + (x - img->pos.x) * (v->z - v->z * MOUSE_SCROL_SCALE);
+			v->y =  v->y + (y - img->pos.y) * (v->z - v->z * MOUSE_SCROL_SCALE);
 			v->z *= MOUSE_SCROL_SCALE;
 		}
-		ft_redraw(param);
+		ft_redraw(param, mngr->cur_img);
 	}
 	return (0);
 }
--- main.c
+++ main.c
@@ -12,8 +12,16 @@
 
 #include <stdio.h>
 #include "fractol.h"
-#define PROGRAM_FILE "mndlbrt.cl"
 
+char *g_kernels[] = {"mndlbrt",
+					 "brnng_shp",
+					 "tantan",
+					 "fabs_tan",
+					 "julia",
+					 "julia_tantan",
+					 "julia_fabstan",
+					 "julia_fabsfabs",
+					 NULL};
 
 void ft_mlx_create_new_window(t_mlx *mlx, t_int2 res, char *name)
 {
@@ -24,36 +32,31 @@ void ft_mlx_create_new_window(t_mlx *mlx, t_int2 res, char *name)
 	mlx->wc++;
 }
 
-static void init(t_mlx *mlx, t_manager *mngr)
-{
-	mlx->mlx_ptr = mlx_init();
-	mlx->wc = 0;
-	mlx->cw = 0;
-	mngr->img_num = 1;
-	mngr->cur_img = 0;
-	mngr->mouse_mask = 0;
-	mngr->key_mask = 0;
-	mngr->imgs[mngr->cur_img].col = (t_float3){0.0, 0.6, 1.0};
-	mngr->imgs[mngr->cur_img].iter_mod = 0;
-	mngr->info = 0;
-}
-
-
-void ft_ocl_make_img(t_img *img, t_ocl *ocl)
+void ft_ocl_make_img(t_img *img, t_ocl *ocl, t_double2 *jc)
 {
 	size_t ls;
 	cl_int err;
 	size_t gs;
+	char test[50];
 
 	ls = img->size_line / 4;
 	gs = img->res.x * img->res.y;
-
-	err = clSetKernelArg(ocl->kernel, 1, sizeof(unsigned int) * 2, &(unsigned int[]){img->iter, ls});
-	err |= clSetKernelArg(ocl->kernel, 2, sizeof(t_double3), &img->start);
-	err |= clSetKernelArg(ocl->kernel, 3, sizeof(t_float3), &img->col);
+	clGetKernelInfo(ocl->kernel[img->kern], CL_KERNEL_FUNCTION_NAME, 50, test, NULL);
+	printf("(%s) %s\n",g_kernels[img->kern], test);
+	printf("start (%f, %f, %f)\n", img->start.x, img->start.y, img->start.z);
+	err = clSetKernelArg(ocl->kernel[img->kern], 0, sizeof(cl_mem), &img->buf);
 	if (err < 0)
 		ft_ocl_err_handler(err, FT_OCL_KERNEL_ARG_ERR);
-	err = clEnqueueNDRangeKernel(ocl->queue, ocl->kernel, 1, NULL, &gs, NULL, 0, NULL, NULL);
+	err = clSetKernelArg(ocl->kernel[img->kern], 1, sizeof(unsigned int) * 2, &(unsigned int[]){img->iter, ls});
+	err |= clSetKernelArg(ocl->kernel[img->kern], 2, sizeof(t_double3), &img->start);
+	err |= clSetKernelArg(ocl->kernel[img->kern], 3, sizeof(t_float3), &img->col);
+	if (img->kern >= JULIA) {
+		printf("start (%f, %f)\n", jc->x, jc->y);
+		err |= clSetKernelArg(ocl->kernel[img->kern], 4, sizeof(t_double2), jc);
+	}
+	if (err < 0)
+		ft_ocl_err_handler(err, FT_OCL_KERNEL_ARG_ERR);
+	err = clEnqueueNDRangeKernel(ocl->queue, ocl->kernel[img->kern], 1, NULL, &gs, NULL, 0, NULL, NULL);
 	if (err < 0)
 		ft_ocl_err_handler(err, FT_OCL_ENQUEUE_KERNEL_ERR);
 	err = clEnqueueReadBuffer(ocl->queue, img->buf, CL_TRUE, 0, sizeof(int) * gs, img->data, 0, NULL, NULL);
@@ -61,62 +64,106 @@ void ft_ocl_make_img(t_img *img, t_ocl *ocl)
 		ft_ocl_err_handler(err, FT_OCL_READ_BUF_ERR);
 }
 
-void ft_ocl_set_env(t_img *img, t_ocl *ocl)
+void ft_ocl_set_env(t_ocl *ocl)
 {
-	size_t gs;
 	cl_int err;
+	int i;
 
-	gs = (size_t)img->res.x * img->res.y;
-	img->buf = clCreateBuffer(ocl->context, CL_MEM_READ_WRITE, sizeof(int) * gs, NULL, &err);
-	if (err < 0)
-		ft_ocl_err_handler(err, FT_OCL_BUFFER_ERR);
-	ocl->queue = clCreateCommandQueue(ocl->context, ocl->device, 0, &err);
-	if (err < 0)
-		ft_ocl_err_handler(err, FT_OCL_QUEUE_ERR);
-	ocl->kernel = clCreateKernel(ocl->program, KERNEL_FUNC, &err);
-	if (err < 0)
-		ft_ocl_err_handler(err, FT_OCL_KERNEL_ERR);
-	err = clSetKernelArg(ocl->kernel, 0, sizeof(cl_mem), &img->buf);
-	if (err < 0)
-		ft_ocl_err_handler(err, FT_OCL_KERNEL_ARG_ERR);
+	ocl->kernel = malloc(sizeof(cl_kernel) * FRACTAL_NUMBER);
+	i = -1;
+	while(g_kernels[++i])
+	{
+		ocl->kernel[i] = clCreateKernel(ocl->program, g_kernels[i], &err);
+		if (err < 0)
+			ft_ocl_err_handler(err, FT_OCL_KERNEL_ERR);
+	}
+}
+
+static void init(t_mlx *mlx, t_manager *mngr, t_ocl *ocl)
+{
+	mlx->mlx_ptr = mlx_init();
+	mlx->wc = 1;
+	mlx->cw = 0;
+	mngr->img_num = 2;
+	mngr->cur_img = MAIN_I;
+	mngr->mouse_mask = 0;
+	mngr->key_mask = 0;
+	mngr->imgs[mngr->cur_img].col = (t_float3){0.0, 0.6, 1.0};
+	mngr->imgs[mngr->cur_img].iter_mod = 0;
+	mngr->info = 0;
+	ft_mlx_create_new_window(mlx, (t_int2){RES + RES / FRACTAL_PRV, RES}, "Fract_ol: main");
+	ft_ocl_dev_cont_prog(ocl, PROGRAM_FILE);
+	ft_ocl_set_env(ocl);
+}
+
+void total_free(void *param)
+{
+	int i;
+
+	i = -1;
+	while(g_kernels[++i])
+		clReleaseKernel(((t_ocl**)param)[0]->kernel[i]);
+	free(((t_ocl**)param)[0]->kernel);
 }
 
 int frct_close(void *param)
 {
-	(void)param;
+	total_free(param);
 	exit(0);
 	return (0);
+}
+
+void init_f_i(t_img *img, t_mlx *mlx, t_double3 start, t_uint2 res, size_t krn, t_ocl *ocl, t_uint2 pos)
+{
+	cl_int err;
+	size_t gs;
+
+	img->img_ptr = mlx_new_image(mlx->mlx_ptr, res.x, res.y);
+	img->data = mlx_get_data_addr(img->img_ptr, &img->bpp, &img->size_line, &img->endian);
+	img->res = res;
+	img->start = start;
+	img->iter = 101;
+	img->pos = pos;
+	if (img->res.x != RES)
+		img->start.z *= FRACTAL_PRV;
+	img->kern = krn;
+	gs = (size_t)img->res.x * img->res.y;
+	img->buf = clCreateBuffer(ocl->context, CL_MEM_READ_WRITE, sizeof(int) * gs, NULL, &err);
+	if (err < 0)
+		ft_ocl_err_handler(err, FT_OCL_BUFFER_ERR);
+	ft_ocl_make_img(img, ocl, &(t_double2){0,0});
 }
 
 int			main(int ac, char **av)
 {
 	t_manager	mngr;
 	t_mlx		mlx;
-	t_img		*img;
+	//t_img		*img;
 	t_ocl		ocl;
 
 	ac = ac + 0;
 	av = av + 0;
-	init(&mlx, &mngr);
-	ft_mlx_create_new_window(&mlx, (t_int2){RES, RES}, "test");
-	//create mandelbrot img
-	//init img
-	img = &mngr.imgs[0];
+	init(&mlx, &mngr, &ocl);
+	init_f_i(&mngr.imgs[MAIN_I], &mlx, MNDLBRT_START, (t_uint2){RES, RES}, MNDLBRT, &ocl, (t_uint2){0, 0});
+	init_f_i(&mngr.imgs[RIGHT1_I], &mlx, FABSTAN_START, (t_uint2){RES / FRACTAL_PRV, RES / FRACTAL_PRV}, FABS_TAN, &ocl, (t_uint2){RES, 0 * RES / FRACTAL_PRV});
+	init_f_i(&mngr.imgs[RIGHT2_I], &mlx, BRNNG_SHP_START, (t_uint2){RES / FRACTAL_PRV, RES / FRACTAL_PRV}, BRNNG_SHP, &ocl, (t_uint2){RES, 1 * RES / FRACTAL_PRV});
+	init_f_i(&mngr.imgs[RIGHT3_I], &mlx, TANTAN_START, (t_uint2){RES / FRACTAL_PRV, RES / FRACTAL_PRV}, TANTAN, &ocl, (t_uint2){RES, 2 * RES / FRACTAL_PRV});
+	init_f_i(&mngr.imgs[RIGHT4_I], &mlx, JULIA_START, (t_uint2){RES / FRACTAL_PRV, RES / FRACTAL_PRV}, JULIA, &ocl, (t_uint2){RES, 3 * RES / FRACTAL_PRV});
+	init_f_i(&mngr.imgs[RIGHT5_I], &mlx, JULIA_START, (t_uint2){RES / FRACTAL_PRV, RES / FRACTAL_PRV}, JULIA_TANTAN, &ocl, (t_uint2){RES, 4 * RES / FRACTAL_PRV});
+	init_f_i(&mngr.imgs[RIGHT6_I], &mlx, JULIA_START, (t_uint2){RES / FRACTAL_PRV, RES / FRACTAL_PRV}, JULIA_FABSTAN, &ocl, (t_uint2){RES, 5 * RES / FRACTAL_PRV});
+	init_f_i(&mngr.imgs[RIGHT7_I], &mlx, JULIA_START, (t_uint2){RES / FRACTAL_PRV, RES / FRACTAL_PRV}, JULIA_FABSFABS, &ocl, (t_uint2){RES, 6 * RES / FRACTAL_PRV});
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr[mlx.cw], mngr.imgs[MAIN_I].img_ptr, 0, 0);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr[mlx.cw], mngr.imgs[RIGHT1_I].img_ptr, RES, mngr.imgs[RIGHT1_I].pos.y);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr[mlx.cw], mngr.imgs[RIGHT2_I].img_ptr, RES, mngr.imgs[RIGHT2_I].pos.y);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr[mlx.cw], mngr.imgs[RIGHT3_I].img_ptr, RES, mngr.imgs[RIGHT3_I].pos.y);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr[mlx.cw], mngr.imgs[RIGHT4_I].img_ptr, RES, mngr.imgs[RIGHT4_I].pos.y);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr[mlx.cw], mngr.imgs[RIGHT5_I].img_ptr, RES, mngr.imgs[RIGHT5_I].pos.y);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr[mlx.cw], mngr.imgs[RIGHT6_I].img_ptr, RES, mngr.imgs[RIGHT6_I].pos.y);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr[mlx.cw], mngr.imgs[RIGHT7_I].img_ptr, RES, mngr.imgs[RIGHT7_I].pos.y);
 
-	img->img_ptr = mlx_new_image(mlx.mlx_ptr, mlx.res[mlx.cw].x, mlx.res[mlx.cw].y);
-	img->data = mlx_get_data_addr(img->img_ptr, &img->bpp, &img->size_line, &img->endian);
-	img->res = (t_uint2){RES, RES};
-	img->start = (t_double3){-2.15, 1.5, 3.0 / RES};
-	img->iter = 101;
-	ft_ocl_dev_cont_prog(&ocl, PROGRAM_FILE);
-	ft_ocl_set_env(img , &ocl);
-	ft_ocl_make_img(img, &ocl);
-	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr[mlx.cw], img->img_ptr, 0, 0);
-
-	//put img in window
 	mlx_hook(mlx.win_ptr[mlx.cw], 2, 5, hook_keydwn, (void*[]){&ocl, &mngr, &mlx});
 	mlx_hook(mlx.win_ptr[mlx.cw], 3, 5, hook_keyrelease, (void*[]){&ocl, &mngr, &mlx});
-	mlx_hook(mlx.win_ptr[mlx.cw], 17, (1L << 3), frct_close, NULL);
+	mlx_hook(mlx.win_ptr[mlx.cw], 17, (1L << 3), frct_close, (void*[]){&ocl, &mngr, &mlx});
 	mlx_hook(mlx.win_ptr[mlx.cw], 4, 0, &mouse_hook, (void*[]){&ocl, &mngr, &mlx});
 	mlx_hook(mlx.win_ptr[mlx.cw], 6, 0, &mouse_move_handle, (void*[]){&ocl, &mngr, &mlx});
 	mlx_hook(mlx.win_ptr[mlx.cw], 5, 0, &mouse_release, (void*[]){&ocl, &mngr, &mlx});
